03e4fcb09e44e8fd3cdcd4b1bd8e7f018e4720ac
larry <lawrence.roberts@ucsf.edu>
2013-10-23 09:28:07 -0700

    added a test and switched jaxb library to central

---
+++ tools/struts-config/src/test/java/StrutsConfigTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.*;
+
+public class StrutsConfigTest
+{
+    @Test
+	public void testCreate()
+	throws Exception
+    {
+	System.out.println("Running a test!");
+	throw new RuntimeException("Not implemeted yet");
+    }
+
+}
